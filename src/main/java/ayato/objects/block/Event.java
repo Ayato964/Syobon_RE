f452ba2b62aab6e0ae56888a4b0127c93e520f4e
Ayato <chtk20063@g.nihon-u.ac.jp>
2023-04-26 21:41:22 +0900

    Ver.Classic.0.18.0
    イベントブロック
    *イベントを発生させるための条件を決める機能を追加
    *イベントのトリガーをオンにする機能を追加。

--- src/main/java/ayato/objects/block/Event.java
+++ src/main/java/ayato/objects/block/Event.java
@@ -9,14 +9,52 @@ import ayato.util.animation.ImageMaker;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.ArrayList;
+import java.util.function.BooleanSupplier;
 
 public class Event extends Block{
-    private boolean isValid = true;
-    private ArrayList<Entity> entities;
+    protected boolean isValid = true;
+    protected boolean trigger = false;
+    private final ArrayList<Entity> entities;
+    private final ArrayList<Integer> subjectIDs;
+    private final ArrayList<BooleanSupplier> conditions;
     public Event(int x, int y, JsonNode eventJson, JsonNode data, int blockID) {
         super(x, y, 1, 1);
         isCollider = false;
         entities = new ArrayList<>();
+        subjectIDs = new ArrayList<>();
+        conditions = new ArrayList<>();
+        triggerSetting(eventJson, data);
+        summonSetting(eventJson, data);
+        conditionSetting(eventJson, data);
+
+
+    }
+
+    private void conditionSetting(JsonNode eventJson, JsonNode data) {
+        JsonNode condition = eventJson.get("condition");
+        if(condition != null){
+            if(!condition.isArray()){
+                conditions.add(condition(condition.asInt()));
+            }
+        }
+    }
+    private BooleanSupplier condition(int id){
+        return switch (id){
+            case 0->TriggerCondition.getType0(this);
+            default -> null;
+        };
+    }
+
+    private void triggerSetting(JsonNode eventJson, JsonNode data) {
+        JsonNode trigger = eventJson.get("trigger");
+        if(trigger != null){
+            if(!trigger.isArray()){
+                subjectIDs.add(trigger.asInt());
+            }
+        }
+    }
+
+    private void summonSetting(JsonNode eventJson, JsonNode data){
         JsonNode summon = eventJson.get("summon");
         if(summon != null)
             if(!summon.isArray()){
@@ -30,29 +68,54 @@ public class Event extends Block{
                         entities.add(e);
                 }
             }
-
     }
 
     @Override
     protected void setAddons(ArrayList<ObjectAddon> addons) {
         addons.add(new InSide(()->(int) x + CodeToon.BLOCK_WIDTH / 2 - 1, ()->(int) y + CodeToon.BLOCK_HEIGHT / 2, ()-> CodeToon.BLOCK_WIDTH / 5, ()->CodeToon.BLOCK_HEIGHT / 5, o -> {
-            if(isValid){
-                isValid = false;
+            if(isValid) {
+                if (getJudgeConditions()) {
+                    isValid = false;
 
-                if(!entities.isEmpty())
-                    stage.endingTask(i -> {
-                        for(Entity e : entities){
-                            e.setX(e.x - stage.stageX);
-                            stage.entities.add(e);
-                        }
-                    });
+                    if (!entities.isEmpty())
+                        stage.endingTask(i -> {
+                            for (Entity e : entities) {
+                                e.setX(e.x - stage.stageX);
+                                stage.entities.add(e);
+                            }
+                        });
+                    if(!subjectIDs.isEmpty()){
+                        stage.endingTask(i -> {
+                            for(int in : subjectIDs){
+                                Block b = stage.blocks.get(in);
+                                if(b instanceof Event)
+                                    ((Event) b).trigger = true;
+                            }
+                        });
+                    }
 
+                }
             }
         }, null));
+    }
+
+    private boolean getJudgeConditions() {
+        for(BooleanSupplier b : conditions){
+            if(!b.getAsBoolean()){
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     protected ImageMaker setTexture() {
         return new ImageMaker("block/blank");
+    }
+
+    protected static class TriggerCondition{
+        public static BooleanSupplier getType0(Event e){
+            return ()-> e.trigger;
+        }
     }
 }
